5a2141a004a4805186bc4caee6a77a020a9ee82a
sivakumar <manchi.104@email.com>
2018-07-29 14:28:32 +0530

    siva

---
+++ siva.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "who is Bavitha lover"
+read name 
+if [ $name == siva ];
+then
+echo "siva is Bavitha lover"
+elif [ $name == naresh ];
+then
+echo "naresh is Bavitha Brother"
+elif [ $name == pavan ];
+then 
+echo "pavan is Bavitha friend"
+fi
